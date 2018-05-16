ceff8527ecec0342d66d24e8a9cb13501c3bd892
Edward White <edward@edwards-mac-mini.local>
2018-05-15 21:53:00 -0500

    initial commit and creation of tableviewcontroller and CoinData class

---
+++ Crypto Tracker/coinData.swift
@@ -0,0 +1,18 @@
+//
+//  coinData.swift
+//  Crypto Tracker
+//
+//  Created by Edward White on 5/15/18.
+//  Copyright © 2018 Edward White. All rights reserved.
+//
+
+import UIKit
+
+class Coin{
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var amount = 0.0
+    var historicalData = [Double]()
+    
+}
