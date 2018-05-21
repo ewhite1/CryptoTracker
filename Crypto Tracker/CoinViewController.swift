1c7f610b04acaa033210e5922701f1305376693a
Edward White <edwardwhite1@outlook.com>
2018-05-21 01:48:48 -0500

    intialization of chart window

---
+++ Crypto Tracker/CoinViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CoinViewController.swift
+//  Crypto Tracker
+//
+//  Created by Edward White on 5/21/18.
+//  Copyright © 2018 Edward White. All rights reserved.
+//
+
+import UIKit
+
+class CoinViewController: UIViewController {
+
+    var coin : Coin?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+
+}
